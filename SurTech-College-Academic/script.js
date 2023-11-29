a9ec4005de30c4cb3ee3f83a403b38b4d6a9cff4
dev-debabrata <debabratadas711@gmail.com>
2023-11-29 08:46:27 +0530

    29-11-2023

--- SurTech-College-Academic/script.js
+++ SurTech-College-Academic/script.js
@@ -1,7 +1,7 @@
 var navLinks = document.getElementById("navLinks");
 function showMenu() {
-    navLinks.style.right = 0;
+  navLinks.style.right = 0;
 }
 function hideMenu() {
-    navLinks.style.right = "-200px";
+  navLinks.style.right = "-200px";
 }
